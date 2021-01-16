703b98f7d313b3dc3406aa7f599f8620477766ea
James Greenhill <fuziontech@gmail.com>
2021-01-15 22:09:12 -0800

    initial commit for funzies

---
+++ env/dev/cljs/myreagent/dev.cljs
@@ -0,0 +1,15 @@
+(ns ^:figwheel-no-load myreagent.dev
+  (:require
+    [myreagent.core :as core]
+    [devtools.core :as devtools]))
+
+(extend-protocol IPrintWithWriter
+  js/Symbol
+  (-pr-writer [sym writer _]
+    (-write writer (str "\"" (.toString sym) "\""))))
+
+(devtools/install!)
+
+(enable-console-print!)
+
+(core/init!)
